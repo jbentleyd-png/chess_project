a0bcb1bb45a0937f150fcbf17046169375cee2fb
Joshua Bentley <232677218+jbentleyd-png@users.noreply.github.com>
2026-07-28 14:01:35 -0600

    Added initialize sub-methods to populate board.
    Team arrays conatain piece objects, spaces point to pieces if occupied

--- lib/board/board.rb
+++ lib/board/board.rb
@@ -10,9 +10,72 @@ require_relative 'pieces/queen'
 require_relative 'pieces/rook'
 
 class Board
+  def space_coordinates
+    spaces = []
+    files = [*1..8]
+    files.each do |c|
+      files.each do |r|
+        spaces << [r, c]
+      end
+    end
+    spaces
+  end
+
+  def populate_red
+    @space_coordinates[0..15].map do |c|
+      case c
+      when [1, 1], [8, 1]
+        Rook.new(c)
+      when [2, 1], [7, 1]
+        Knight.new(c)
+      when [3, 1], [6, 1]
+        Bishop.new(c)
+      when [4, 1]
+        Queen.new(c)
+      when [5, 1]
+        King.new(c)
+      else
+        Pawn.new(c)
+      end
+    end
+  end
+
+  def populate_blue
+    @space_coordinates[48..63].map do |c|
+      case c
+      when [1, 8], [8, 8]
+        Rook.new(c, 'blue')
+      when [2, 8], [7, 8]
+        Knight.new(c, 'blue')
+      when [3, 8], [6, 8]
+        Bishop.new(c, 'blue')
+      when [4, 8]
+        Queen.new(c, 'blue')
+      when [5, 8]
+        King.new(c, 'blue')
+      else
+        Pawn.new(c, 'blue')
+      end
+    end
+  end
+
+  def populate_board
+    @space_coordinates.map.with_index do |c, i|
+      case i
+      when 0..15
+        Space.new(c, @red_team[i])
+      when 16..47
+        Space.new(c)
+      when 48..63
+        Space.new(c, @blue_team[i - 48])
+      end
+    end
+  end
+
   def initialize
-    @spaces = []
-    @white_team = []
-    @black_team = []
+    @space_coordinates = space_coordinates
+    @red_team = populate_red # == white
+    @blue_team = populate_blue # == black
+    @spaces = populate_board
   end
 end
--- spec/board/board_spec.rb
+++ spec/board/board_spec.rb
@@ -2,9 +2,74 @@
 
 require_relative '../../lib/board/board'
 describe Board do
-  describe '' do
-    it '' do
-      expect(true).to eq(true)
+  describe 'space_coordinates' do
+    it 'creates 64 coordinates' do
+      test = Board.new
+      sc = test.space_coordinates
+      p sc
+      puts 'First 16:'
+      p sc[0..15]
+      expect(sc.length).to eq(64)
+    end
+
+    it '7th space is [7, 1]' do
+      test = Board.new
+      sc = test.space_coordinates
+      expect(sc[6]).to eq([7, 1])
+    end
+  end
+
+  describe 'populate_red' do
+    it 'creates 16 pieces' do
+      test = Board.new
+      pr = test.populate_red
+      expect(pr.length).to eq(16)
+    end
+
+    it 'third piece is a bishop' do
+      test = Board.new
+      pr = test.populate_red
+      expect(pr[2]).to be_a(Bishop)
+    end
+  end
+
+  describe 'populate_blue' do
+    it 'creates 16 pieces' do
+      test = Board.new
+      pb = test.populate_blue
+      expect(pb.length).to eq(16)
+    end
+
+    it '11th piece is a bishop' do
+      test = Board.new
+      pb = test.populate_blue
+      expect(pb[10]).to be_a(Bishop)
+    end
+  end
+
+  describe 'populate_board' do
+    it 'final Space is occupied by a Rook' do
+      test = Board.new
+      pb = test.populate_board
+      expect(pb[-1].occupied_by).to be_a(Rook)
+    end
+
+    it '[4,4] is empty' do
+      test = Board.new
+      pb = test.populate_board
+      four_four_index = pb.find_index { |space| space.name == [4, 4] }
+      p four_four_index
+
+      expect(pb[four_four_index].occupied_by).to eq(nil)
+    end
+
+    it '[2,8] has a knight on it' do
+      test = Board.new
+      pb = test.populate_board
+      four_four_index = pb.find_index { |space| space.name == [2, 8] }
+      p four_four_index
+
+      expect(pb[four_four_index].occupied_by).to be_a(Knight)
     end
   end
 end
