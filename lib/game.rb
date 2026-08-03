746070c727a5d67b65e12fa1c8ecd746bcb6a8bb
Joshua Bentley <232677218+jbentleyd-png@users.noreply.github.com>
2026-08-02 19:45:12 -0600

    Refactored input checking for memory, specificity.
    Now a bad input will tell you why it was bad. Saved dupl. assignments.

--- lib/game.rb
+++ lib/game.rb
@@ -11,21 +11,8 @@ class Game
   end
 
   # INPUT CONVERSION:
-  def letter_convert(coordinate)
-    output = ''
-    letter_list = %w[A B C D E F G H]
-    output += letter_list[coordinate[0] - 1]
-    output += coordinate[1].to_s
-    output
-  end
-
-  def valid_row?(letter)
-    %w[A B C D E F G H].include?(letter[0].upcase)
-  end
 
   def number_convert(chess_space)
-    return 'invalid row' unless valid_row?(chess_space)
-
     letter_list = %w[A B C D E F G H]
     arr_version = chess_space.chars
     col = letter_list.index(arr_version[0]) + 1
@@ -34,21 +21,25 @@ class Game
     arr_version
   end
 
-  # SLECTING THE PIECE:
-  def turn_message
-    first = @turn == 'red' ? "Red's turn. ".red : "Yellow's turn. ".yellow
-    "#{first}\nWhich piece would you like to move?\n#{'Select using coordinates (A1, G4, etc.).'.gray}\nEnter: "
+  # VALIDATING MOVING PIECE SELECTION:
+
+  def valid_row?(input)
+    %w[A B C D E F G H].include?(input[0].upcase)
   end
 
-  def piece_on_team?(piece_coordinate)
-    space = @board.spaces.find { |s| s.name == piece_coordinate }
+  def space_on_board?(input)
+    return false unless valid_row?(input)
+
+    @board.space_coordinates.include?(number_convert(input))
+  end
+
+  def piece_on_team?(space)
     return false if space.occupied_by.nil?
 
     space.occupied_by.team == @turn
   end
 
-  def piece_can_move?(piece_coordinate) # this one requires some data fixes
-    space = @board.spaces.find { |s| s.name == piece_coordinate }
+  def piece_can_move?(space)
     piece = space.occupied_by
     total_adj_moves = piece.adjacent_moves.length
     blocked = 0
@@ -59,26 +50,61 @@ class Game
     blocked < total_adj_moves
   end
 
-  def reselect_piece
+  def reselect_message(issue)
+    message = case issue
+              when 'off_board'
+                'on the board.'
+              when 'not_your_piece'
+                'your own piece.'
+              else
+                'movable.'
+              end
     puts 'Enter a valid coordinate.'.red
-    puts 'Coordinate must be on the board and your own piece, which can move.'.gray
-    print 'Enter: '
-    number_convert(gets.chomp.upcase)
+    print "#{'Coordinate must be '.gray}#{message.gray}\nEnter: "
   end
 
-  def select_piece
-    # player_team = @turn == 'red' ? @baord.red_team : @board.blue_team
-    piece_coordinate = number_convert(gets.chomp.upcase)
-    until @board.space_coordinates.include?(piece_coordinate) && piece_on_team?(piece_coordinate) && piece_can_move?(piece_coordinate)
-      piece_coordinate = reselect_piece
+  # SELECTING THE PIECE TO MOVE:
+
+  def select_space(input)
+    piece_coordinate = number_convert(input)
+    @board.spaces.find { |s| s.name == piece_coordinate } # returns nil if nothing is found
+  end
+
+  def select_start_space
+    space = nil
+    loop do
+      input = gets.chomp.upcase
+      unless space_on_board?(input)
+        reselect_message('off_board')
+        next
+      end
+
+      space = select_space(input)
+      unless piece_on_team?(space)
+        reselect_message('not_your_piece')
+        next
+      end
+      unless piece_can_move?(space)
+        reselect_message("can't move")
+        next
+      end
+      break
     end
+    space
   end
 
   # MAKING A MOVE:
+  def turn_message
+    first = @turn == 'red' ? "Red's turn. ".red : "Yellow's turn. ".yellow
+    "#{first}\nWhich piece would you like to move?\n#{'Select using coordinates (A1, G4, etc.).'.gray}\nEnter: "
+  end
+
   def make_move
     @board.render(@turn)
     print turn_message
-    piece = select_piece
+    start_space = select_start_space
+    piece = start_space.occupied_by
+    p piece
   end
 
   def play
